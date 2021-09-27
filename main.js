37b9eeb6421ba036451780308bc5bfd5cdc41f7a
Josema <enzo@josema.es>
2021-09-27 09:00:25 +0200

    Bug fixed

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 // main.js
 let pathname;
+
 document.body.onload = () => {
-  async function onChangePath(pathname) {
+  function checkPath() {
+    if (pathname !== location.pathname) {
+      pathname = location.pathname;
+      onChangePath(pathname);
+    }
+  }
+
+  function onChangePath(pathname) {
     if (pathname === "/league") {
       showBalance();
     }
   }
 
-  function showBalance() {
+  async function showBalance() {
     const { data } = await fetchData(
-      "https://biwenger.as.com/api/v2/league?include=all&fields=*,standings,tournaments,group,settings(description)"
+      "/league?include=all&fields=*,standings,tournaments,group,settings(description)"
     );
     Array.from(document.getElementsByTagName("balance")).forEach(
       (element, index) => {
@@ -24,24 +32,17 @@ document.body.onload = () => {
     );
   }
 
-  function checkPath() {
-    if (pathname !== location.pathname) {
-      pathname = location.pathname;
-      onChangePath(pathname);
-    }
-  }
-
   checkPath();
   setInterval(checkPath, 1000);
 };
 
-async function fetchData(url) {
+async function fetchData(endpoint) {
   const lastSession = JSON.parse(localStorage.getItem("lastSession"));
   const token = localStorage.getItem("satellizer_token");
   const locale = localStorage.getItem("locale");
   const league = localStorage.getItem("league");
 
-  const response = await fetch(url, {
+  const response = await fetch("https://biwenger.as.com/api/v2" + endpoint, {
     headers: {
       accept: "application/json, text/plain, */*",
       "content-type": "application/json; charset=utf-8",
